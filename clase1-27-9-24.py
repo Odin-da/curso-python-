e818add83609d94249721fcbdcb29f2558b8eaf5
odin-da <odazacorredor@gmail.com>
2024-09-27 13:02:12 -0500

    creando carpeta

---
+++ clase1-27-9-24.py
@@ -0,0 +1,15 @@
+#edad=int(input("Ingrese la edad"))
+#print("La edad ingresada es:",edad)
+#print(type(edad)) 
+"""
+nombre=input("Ingrese su nombre:")
+print(nombre)
+print(type(nombre))
+peso=float(input("Ingrese su peso :"))
+print(peso)
+print(type(peso))
+"""
+
+condicion=bool(input("Ingrese su condicion :"))
+print(condicion)
+print(type(condicion))
